79dab54ff90fd59e326e139a64709a796e0d416b
Kyle Schoenberger <kyleschoen@gmail.com>
2020-07-17 00:08:13 -0700

    img upload plugin install

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,33 @@ module.exports = {
     },
   },
   `gatsby-transformer-remark`,
+  {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
+          },
+        ],
+      },
+    },
   ],
 }
