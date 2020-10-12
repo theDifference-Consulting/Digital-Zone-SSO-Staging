3127312bd2a77105f9dab0f56bb7569ac8053616
kyleschoenberger <kyleschoen@gmail.com>
2020-10-12 09:38:39 -0700

    Update drive-in.js

--- src/pages/drive-in.js
+++ src/pages/drive-in.js
@@ -53,73 +53,36 @@ export default function index() {
                     <img src="../img/popcorn-bug.svg" className="bug"/>
                     <h2>Now Showing</h2>
                     <div style={{padding:'30px 0'}}>
-                        Learn how the Middle East Medical Affairs team uses a digital platform to expand its reach to local HIV specialists, provide them with much needed scientific information and learn about their interests and behaviors.
+                        Learn about the way HIV experts in Australia have adapted to stay connected and share their expertise with each other.
                     </div>
                     <div className="yellow-video-block">
                         <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/463555097" width="640" height="360" frameBorder="0" allow="fullscreen" allowFullScreen></iframe>
+                            <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/465969769" width="640" height="360" frameBorder="0" allow="fullscreen" allowFullScreen></iframe>
                         </div>
-                        <b>Sherif Khattab</b><br/>
-                        Dubai, UAE<br/>
-                        sherif.khattab@gilead.com
+                        <b>Julie Newman</b><br/>
+                        Australia<br/>
+                        julie.newman@gilead.com
                     </div>
                     
-                    {
-                    // <div class="row margin-top-md">
-                    //     <div className="col-sm-6 ">
-                    //         <div className="yellow-video-block">
-                    //             <h3>Video Title</h3>
-                    //                 <div className="embed-responsive embed-responsive-16by9">
-                    //                     <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/xxx" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                    //                 </div>
-                    //             <p>
-                    //                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ...
-                    //             </p>
-                    //         </div>
-                    //         <img src="../img/headshot-bottom-bg.svg" className="yellow-video-block-img"/>
-                    //     </div>
-                    //     <div className="col-sm-6 ">
-                    //         <div className="yellow-video-block">
-                    //             <h3>Video Title</h3>
-                    //                 <div className="embed-responsive embed-responsive-16by9">
-                    //                     <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/xxx" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                    //                 </div>
-                    //             <p>
-                    //                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ...
-                    //             </p>
-                    //         </div>
-                    //         <img src="../img/headshot-bottom-bg.svg" className="yellow-video-block-img"/>
-                    //     </div>
-                    //     <div className="col-sm-6 ">
-                    //         <div className="yellow-video-block">
-                    //             <h3>Video Title</h3>
-                    //                 <div className="embed-responsive embed-responsive-16by9">
-                    //                     <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/xxx" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                    //                 </div>
-                    //             <p>
-                    //                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ...
-                    //             </p>
-                    //         </div>
-                    //         <img src="../img/headshot-bottom-bg.svg" className="yellow-video-block-img"/>
-                    //     </div>
-                    //     <div className="col-sm-6 ">
-                    //         <div className="yellow-video-block">
-                    //             <h3>Video Title</h3>
-                    //                 <div className="embed-responsive embed-responsive-16by9">
-                    //                     <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/xxx" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                    //                 </div>
-                    //             <p>
-                    //                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ...
-                    //             </p>
-                    //         </div>
-                    //         <img src="../img/headshot-bottom-bg.svg" className="yellow-video-block-img"/>
-                    //     </div>
-                    // </div>
-                }
+                    
+                    <div class="row margin-top-md">
+                        <div className="col-sm-6 ">
+                            <div className="yellow-video-block">
+                                <p>
+                                    Learn how the Middle East Medical Affairs team uses a digital platform to expand its reach to local HIV specialists, provide them with much needed scientific information and learn about their interests and behaviors.
+                                </p>
+                                    <div className="embed-responsive embed-responsive-16by9">
+                                        <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/463555097" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                                    </div>
+                                <b>Sherif Khattab</b><br/>
+                                Dubai, UAE<br/>
+                                sherif.khattab@gilead.com
+                            </div>
+                            <img src="../img/headshot-bottom-bg.svg" className="yellow-video-block-img"/>
+                        </div>
+                    </div>
                 </div>
             </section>
-           
-
 	        <footer>
 	            <div className="container">
 	                <div className="copyright">©2020 Gilead</div>
