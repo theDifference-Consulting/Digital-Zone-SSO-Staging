1cba638af6607bdb168153eb12579367adfcbf64
kyleschoenberger <kyleschoen@gmail.com>
2021-04-20 17:37:06 -0700

    Create form-test.js

---
+++ src/pages/form-test.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { Link } from "gatsby"
+import Navbar from "../components/navBar"
+import Layout from "../components/layout"
+
+export default function index() {
+    return (
+        <Layout pageName="form test" activePage=" " containerClass=" ">
+            <div className="container" style={{marginTop: "60px", marginBottom: "40%"}}>
+                <form name="contact" method="POST" data-netlify="true">
+								  <p>
+								    <label>Your Name: <input type="text" name="name" /></label>   
+								  </p>
+								  <p>
+								    <label>Your Email: <input type="email" name="email" /></label>
+								  </p>
+								  <p>
+								    <label>Your Role: <select name="role[]" multiple>
+								      <option value="leader">Leader</option>
+								      <option value="follower">Follower</option>
+								    </select></label>
+								  </p>
+								  <p>
+								    <label>Message: <textarea name="message"></textarea></label>
+								  </p>
+								  <p>
+								    <button type="submit">Send</button>
+								  </p>
+								</form>
+            </div>
+        </Layout>
+	)
+}
