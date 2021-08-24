13ff24a6144d5145c0b0f2b9ee9b076e4654431b
kyleschoenberger <kyleschoen@gmail.com>
2021-08-24 09:25:07 -0700

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -15,7 +15,6 @@ const CustomNavbar = ({ pageInfo, ...props }) => {
           <Nav.Link eventKey="drive-in" href="/drive-in/">Drive-Innovation Theater</Nav.Link>
           <Nav.Link eventKey="library" href="/library/">Library</Nav.Link>
           <Nav.Link eventKey="newsletter" href="/newsletter-depot/">Newsletter Depot</Nav.Link>
-          <Nav.Link eventKey="epic-quest" href="/epic-quest/">Epic Quest</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
