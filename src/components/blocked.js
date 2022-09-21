192d5ad0058af0ef21de7a64a420ec535c52a562
Joaquín Omar Tumas <j.tumas@gmail.com>
2022-09-21 15:15:28 -0300

    add more images for different sizes

--- src/components/blocked.js
+++ src/components/blocked.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export const Blocked = () => {
   return (
-    <div style={{ "width": "100%", height: "100vh", backgroundColor: "#C5203F", display :"flex", "justifyContent": "center", "alignItems": "center" }}>
-      <img srcSet="/img/blocked-bg.jpg" style={{"maxWidth": "100%", maxHeight: "100%"}} />
+    <div style={{ "width": "100%", height: "100vh", backgroundColor: "#C5203F", display :"flex", "justifyContent": "center", "alignItems": "center", overflow: "hidden" }}>
+      <img 
+        srcSet="/img/blocked-bg-3.jpg 1667w, /img/blocked-bg-2.jpg 1043w, /img/blocked-bg.jpg 2917w"
+        src="/img/blocked-bg.jpg"
+        style={{"maxWidth": "100%", maxHeight: "100%"}}
+      />
     </div>
   )
 }
